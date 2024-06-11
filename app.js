ab5e7d31a46c820135edf082a5564bb17b475c8c
Dtan713 <s.donaldtan@gmail.com>
2024-06-11 09:59:40 -0400

    added more

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ let counter = 0;
 
 function incrementCounter() {
     counter++;
+    console.log(counter)
     incrementCounter()
 
 }
@@ -13,4 +14,22 @@ try {
     console.log(error,counter)
     
 }
+
+
+const array = [1, 2, [3, 4], 5, [6, 7]]];
+
+let newArr = [];
+
+function flatten(index, arr) {
+    if (index >= arr.length) return newArr;
+
+    if (array.isArray(arr[index])) {
+        flatten(0, arr[index]);
+    } else {
+        newArr.push(arr[index]);
+    }
+
+    return () => flatten(index + 1, arr);
+}
+
 
